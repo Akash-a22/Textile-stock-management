c2257ba2c11d7452514f7aeefd4684520cf9763f
akash3003 <aj980947@gmail.com>
2023-11-13 09:57:48 +0530

    project is created

---
+++ src/main/java/com/tyss/textilemanagement/TestileStockManagement/TestileStockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tyss.textilemanagement.TestileStockManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestileStockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestileStockManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyss/textilemanagement/TestileStockManagement/TestileStockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyss.textilemanagement.TestileStockManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestileStockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
